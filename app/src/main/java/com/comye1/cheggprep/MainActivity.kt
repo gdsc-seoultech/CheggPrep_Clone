3162d1728ac09ed5334ff53564f04c38757f3f31
comye1 <comye1@seoultech.ac.kr>
2021-09-15 22:31:47 +0900

    FirstWeek

---
+++ app/src/main/java/com/comye1/cheggprep/MainActivity.kt
@@ -0,0 +1,290 @@
+package com.comye1.cheggprep
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Computer
+import androidx.compose.material.icons.filled.NoteAdd
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.comye1.cheggprep.ui.theme.CheggPrepTheme
+import com.comye1.cheggprep.ui.theme.DeepOrange
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CheggPrepTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Column(modifier = Modifier.padding(8.dp)) {
+        DeckInSubject()
+        Spacer(modifier = Modifier.height(8.dp))
+        StudyGuide()
+        Spacer(modifier = Modifier.height(8.dp))
+        DeckItem()
+        Spacer(modifier = Modifier.height(8.dp))
+        MyDeckItem()
+        Spacer(modifier = Modifier.height(8.dp))
+        MakeMyDeck()
+        Spacer(modifier = Modifier.height(8.dp))
+        SubjectItem()
+        Spacer(modifier = Modifier.height(8.dp))
+        CardItem()
+    }
+}
+
+@Composable
+fun DeckInSubject() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = Color.LightGray
+            )
+            .clickable {
+
+            }
+            .padding(16.dp)) {
+        Text(
+            text = "recursion",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "8 Cards",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+fun StudyGuide() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = Color.LightGray
+            )
+            .clickable {
+
+            }
+            .padding(16.dp)) {
+        Text(
+            text = "c-plus-plus",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "12 Decks · 207 Cards",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+    }
+}
+
+@Composable
+fun DeckItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = Color.LightGray
+            )
+            .clickable {
+
+            }
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "recursion",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "8 Cards",
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+            Icon(
+                imageVector = Icons.Default.Bookmark,
+                contentDescription = "bookmark",
+                tint = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun MyDeckItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                color = Color.LightGray
+            )
+            .clickable {
+
+            }
+            .padding(16.dp)) {
+        Text(
+            text = "recursion",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "11 Cards",
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color.Gray
+            )
+            Icon(
+                imageVector = Icons.Default.VisibilityOff,
+                contentDescription = "visibility_off",
+                tint = Color.Gray
+            )
+        }
+
+    }
+}
+
+@Composable
+fun MakeMyDeck() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .border(
+            width = 2.dp,
+            color = Color.LightGray
+        )
+        .clickable {
+
+        }
+        .padding(20.dp)) {
+        Text(
+            text = "Make your own cards",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "It's easy to create your own flashcard deck -for free.",
+            style = MaterialTheme.typography.subtitle1,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Icon(
+                imageVector = Icons.Default.NoteAdd,
+                contentDescription = "bookmark",
+                tint = Color.Blue
+            )
+            Text(
+                text = "Get started",
+                style = MaterialTheme.typography.subtitle1,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
+
+@Composable
+fun SubjectItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                shape = RoundedCornerShape(size = 8.dp),
+                width = 2.dp,
+                color = Color.LightGray
+            )
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Computer,
+            contentDescription = "bookmark",
+            tint = DeepOrange,
+            modifier = Modifier.size(36.dp)
+        )
+        Text(
+            text = "  Computer Science",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun CardItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 2.dp, color = Color.LightGray)
+    ) {
+        Text(
+            text = "Operating Systems",
+            modifier = Modifier.padding(16.dp),
+            fontWeight = FontWeight.ExtraBold
+        )
+        Divider(modifier = Modifier
+            .fillMaxWidth()
+            .height(2.dp), color = Color.LightGray)
+        Text(
+            text = "A request to execute an OS service-layer function",
+            modifier = Modifier.padding(16.dp),
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
