cc27fc5a7957555a9aa981ce2fa73e87d84efc76
comye1 <comye1@seoultech.ac.kr>
2022-01-24 18:48:08 +0900

    Firebase RDB 입출력할 클래스 작성

--- app/src/main/java/com/comye1/cheggprep/models/Deck.kt
+++ app/src/main/java/com/comye1/cheggprep/models/Deck.kt
@@ -5,8 +5,21 @@ data class Deck(
     val deckTitle: String,
     val shared: Boolean,
     val bookmarked: Boolean,
-    val cardList: List<Card>
+    val cardList: List<Card>,
+    val path: String = ""
 )
 
 const val DECK_CREATED = 0
 const val DECK_ADDED = 1
+
+data class DeckForAll( // Firebase All에 저장될 클래스
+    val deckTitle: String,
+    val shared: Boolean,
+    val cardList: List<Card>
+)
+
+data class DeckForUser( // Firebase User에 저장될 클래스
+    val deckType: Int,
+    val bookmarked: Boolean,
+    val path: String,
+)
