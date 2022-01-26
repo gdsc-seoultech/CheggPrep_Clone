7e4ab545f6ad32bf163976f6e6aa73728b100ecb
comye1 <comye1@seoultech.ac.kr>
2022-01-26 15:01:07 +0900

    Search 결과로 보여줄 Deck들을 RDB에서 불러오기 - 완료!

--- app/src/main/java/com/comye1/cheggprep/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/comye1/cheggprep/viewmodel/SearchViewModel.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import com.comye1.cheggprep.SampleDataSet
+import com.comye1.cheggprep.models.Card
 import com.comye1.cheggprep.models.Deck
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
-class SearchViewModel: ViewModel() {
+class SearchViewModel : ViewModel() {
     // SearchScreen ////////////////////////
     var searchScreenState = mutableStateOf(SearchState.ButtonScreen)
         private set
@@ -43,8 +45,34 @@ class SearchViewModel: ViewModel() {
     }.toMutableStateList()
     /////////////////////////////////
 
-    init {
+    companion object {
+        /*
+        가져온다..!
+         */
+        val database = Firebase.database.reference
 
-        totalDeckList = SampleDataSet.totalDeckSample.toMutableStateList()
+    }
+
+    init {
+        /*
+        All안에 있는 Deck들 가져오기... 업뎃은 노
+         */
+        database.child("all/decks").get().addOnSuccessListener { all ->
+            val decks = all.value as HashMap<String, *>
+//            Log.d("search firebase", deckForAll.keys.toString())
+            decks.keys.forEach { key ->
+                val deckForAll = decks[key] as HashMap<*, *>
+                totalDeckList.add(
+                    Deck(
+                        deckType = -1,
+                        deckTitle = deckForAll["deckTitle"] as String,
+                        cardList = deckForAll["cardList"] as List<Card>,
+                        bookmarked = false,
+                        shared = deckForAll["shared"] as Boolean,
+                        key = key
+                    )
+                )
+            }
+        }
     }
 }
