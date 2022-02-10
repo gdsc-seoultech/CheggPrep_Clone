dc5f36790de3490df04f56b655e92a84adb7e5e5
comye1 <comye1@seoultech.ac.kr>
2022-02-10 12:14:23 +0900

    #1 SearchViewModel - getAllDecks, queryResult

--- app/src/main/java/com/comye1/cheggprep/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/comye1/cheggprep/viewmodel/SearchViewModel.kt
@@ -3,6 +3,7 @@ package com.comye1.cheggprep.viewmodel
 import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.comye1.cheggprep.models.Card
@@ -36,49 +37,49 @@ class SearchViewModel : ViewModel() {
     }
 
     // 전체 Deck
-    var totalDeckList = mutableStateListOf<Deck>()
-        private set
+    private var totalDeckList = mutableStateListOf<Deck>()
 
     // Deck 검색 결과 반환
-    fun getQueryResult() = totalDeckList.filter { deck ->
-        deck.deckTitle.lowercase(Locale.getDefault())
-            .contains(queryString.value.lowercase(Locale.getDefault()))
-    }.toMutableStateList()
+    val queryResult: SnapshotStateList<Deck>
+        get() = totalDeckList.filter { deck ->
+            deck.deckTitle.lowercase(Locale.getDefault())
+                .contains(queryString.value.lowercase(Locale.getDefault()))
+        }.toMutableStateList()
 
     companion object {
-        /*
-        가져온다..!
-         */
         val database = Firebase.database.reference
-
     }
 
-    init {
+    fun getAllDecks() {
         /*
         All안에 있는 Deck들 가져오기... 업뎃은 노
+        valueEventListener로 바꾼다 or 검색 요청마다 불러오기
+        => 검색 요청마다 불러오기
          */
         database.child("all/decks").get().addOnSuccessListener { all ->
-            Log.d("search firebase", all.toString())
+            Log.d("searchscreen", "called")
             /*
             아무것도 없을 때..아무것도 안 보여줌..
              */
+            totalDeckList.clear()
             if (all.value != null) {
                 val decks = all.value as HashMap<String, *>
                 decks.keys.forEach { key ->
                     val deckForAll = decks[key] as HashMap<*, *>
-                    // 사용자의 Deck 이거나 (deckType == 0)
-                    // 다른 사용자의 Deck 이면서 공유된 것 (shared == true)
-                    // TODO 다른 뷰모델에서 가져와야 하는건가 로직짜기 어렵다!!!!!!!!111
-                    totalDeckList.add(
-                        Deck(
-                            deckType = -1,
-                            deckTitle = deckForAll["deckTitle"] as String,
-                            cardList = deckForAll["cardList"] as List<Card>,
-                            bookmarked = false,
-                            shared = deckForAll["shared"] as Boolean,
-                            key = key
+                    // (shared == true)
+                    val shared = deckForAll["shared"] as Boolean
+                    if (shared) {
+                        totalDeckList.add(
+                            Deck(
+                                deckType = -1,
+                                deckTitle = deckForAll["deckTitle"] as String,
+                                cardList = deckForAll["cardList"] as List<Card>,
+                                bookmarked = false,
+                                shared = shared,
+                                key = key
+                            )
                         )
-                    )
+                    }
                 }
             }
         }
