e44e2469a01081bce7a7b0d80ed290e52b231a11
comye1 <comye1@seoultech.ac.kr>
2022-02-24 00:57:26 +0900

    #4 AuthResultContract 수정

--- app/src/main/java/com/comye1/cheggprep/utils/AuthResultContract.kt
+++ app/src/main/java/com/comye1/cheggprep/utils/AuthResultContract.kt
@@ -9,13 +9,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 
 class AuthResultContract: ActivityResultContract<Int, Task<GoogleSignInAccount>?>() {
-    override fun createIntent(context: Context, input: Int?): Intent =
-        getGoogleSignInClient(context).signInIntent.putExtra("input", input)
-
     override fun parseResult(resultCode: Int, intent: Intent?): Task<GoogleSignInAccount>? {
         return when(resultCode) {
             Activity.RESULT_OK -> GoogleSignIn.getSignedInAccountFromIntent(intent)
             else -> null
         }
+    }
+
+    override fun createIntent(context: Context, input: Int): Intent {
+        return getGoogleSignInClient(context).signInIntent.putExtra("input", input)
     }
 }
